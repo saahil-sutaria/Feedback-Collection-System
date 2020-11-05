439cbd4cea8e5e042458a81a596c7ca36490687c
Sahil Sutaria <45413085+saahil-sutaria@users.noreply.github.com>
2020-11-04 17:20:24 -0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -5,7 +5,7 @@ module.exports = {
     mongoURI: 'mongodb+srv://admin:admin@cluster0.4ueaf.mongodb.net/emaily-dev?retryWrites=true&w=majority',
     cookieKey : 'asjkajdfahsidufhaisuhoiuhiaudhs',
     stripePublishableKey : 'pk_test_51HhVALEfM1NIu9w5bzExkFnJ6rzN8M5mpx05BYLnmJTERPnEd3kydephDimB99XY2pLwUom4G7B8MNm0hZxdRHPW00n68jBddD',
-    stripeSecretKey: 'sk_test_51HhVALEfM1NIu9w5rLr6yd6Mup5fkpIysBnVro2xMLOgrO1VSSvmQ1weTg6T6pYcnfDzlhZwAgfDz0kEbAd9HDfU00O5XpotEF',
+    stripeSecretKey: '',
     sendGridKey : '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',
     redirectDomain: 'http://localhost:3000'
     
