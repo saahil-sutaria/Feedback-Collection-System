3b1f2fb81012c5e107570455a1b1751773e680c6
Sahil Sutaria <45413085+saahil-sutaria@users.noreply.github.com>
2020-11-04 17:21:32 -0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,12 +1,12 @@
 //don't commit this
 module.exports = {
-    googleClientID: '309243339046-a64mir112jvg5lh1uvjpjnqh2318ngca.apps.googleusercontent.com',
-    googleClientSecret: 'GHromnwTU3EX966n-WVACC0E',
-    mongoURI: 'mongodb+srv://admin:admin@cluster0.4ueaf.mongodb.net/emaily-dev?retryWrites=true&w=majority',
-    cookieKey : 'asjkajdfahsidufhaisuhoiuhiaudhs',
-    stripePublishableKey : 'pk_test_51HhVALEfM1NIu9w5bzExkFnJ6rzN8M5mpx05BYLnmJTERPnEd3kydephDimB99XY2pLwUom4G7B8MNm0hZxdRHPW00n68jBddD',
+    googleClientID: '',
+    googleClientSecret: '',
+    mongoURI: '',
+    cookieKey : '',
+    stripePublishableKey : '',
     stripeSecretKey: '',
-    sendGridKey : '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',
+    sendGridKey : '',
     redirectDomain: 'http://localhost:3000'
     
 }
